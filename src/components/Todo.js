eba42587ec80db83d35063ff836b3cd0b18dbc87
hoangdv99 <hoangdv257@gmail.com>
2021-04-08 16:36:56 +0000

    Save todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -20,13 +20,15 @@ import {getKey} from "../lib/util";
 
 function Todo() {
   const [input, setInput] = useState("");
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
+  
+  const [items, putItems, clearItems] = useStorage();
   
   const [filter, setFilter] = useState("ALL");
   const handleFilterChange = (value) => {
